d9784d8071504cd21e8c2a5211787bd7b2cc3646
Daniel Royer <2CATteam@gmail.com>
2020-11-14 00:52:42 -0600

    Fixed outstanding letters (I think)

---
+++ ccw_converter.py
@@ -0,0 +1,51 @@
+toChange = input("Paste SVG data\n")
+
+arr = toChange.split()
+
+stack = []
+
+commands = ["M", "V", "H", "C", "L", "Q"]
+
+toAdd = []
+
+toPrint = ""
+
+tempx = 0
+tempy = 0
+
+for data in arr:
+    if (data in commands):
+        stack.append(toAdd)
+        toAdd = [data]
+    elif (data == "Z"):
+        stack = stack [::-1]
+        while len(stack) > 0:
+            adding = stack.pop(0)
+            if len(adding) == 0:
+                continue
+            if (adding[0] == "C"):
+                a, b = adding[1], adding[2]
+                adding[1] = adding[3]
+                adding[2] = adding[4]
+                adding[3] = a
+                adding[4] = b
+                if (len(stack[0]) >= 7):
+                    adding[5] = stack[0][5]
+                    adding[6] = stack[0][6]
+            if (adding[0] == "Q"):
+                #a, b = adding[1], adding[2]
+                #adding[1] = adding[3]
+                #adding[2] = adding[4]
+                #adding[3] = a
+                #adding[4] = b
+                if (len(stack) > 0 and len(stack[0]) >= 5):
+                    adding[3] = stack[0][3]
+                    adding[4] = stack[0][4]
+            for item in adding:
+                toPrint += str(item)
+                toPrint += " "
+        toPrint += "Z "
+    else:
+        toAdd.append(data)
+
+print(toPrint)
--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 import * as THREE from './three/build/three.module.js'
 import { SVGLoader } from './three/examples/jsm/loaders/SVGLoader.js'
 import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js'
+import { letterData } from "./letterData.js"
 
-var camera, controls, scene, renderer
+var camera, controls, scene, renderer, loader
 
-function main() {
+var construction = {}
+
+async function main() {
     const canvas = document.querySelector('#c')
     const style = getComputedStyle(document.querySelector('body'))
     canvas.width = parseInt(style.getPropertyValue('width'))
@@ -27,62 +30,108 @@ function main() {
     //Create scene
     scene = new THREE.Scene()
 
-    //Create geometry
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshBasicMaterial({color: 0x00FFFF})
-    const cube = new THREE.Mesh(geometry, material)
-    //scene.add(cube)
+    const ambient = new THREE.AmbientLight(0x101010)
+    scene.add(ambient)
+    const light1 = new THREE.PointLight(0xffaaaa, 2, 0, 2)
+    light1.position.set(30, 80, 50)
+    scene.add(light1)
+    const light2 = new THREE.PointLight(0xffaaaa, 1, 0, 2)
+    light2.position.set(-70, 80, 50)
+    scene.add(light2)
 
     //Show axes
     const axesHelper = new THREE.AxesHelper(4)
     scene.add(axesHelper)
 
-    const loader = new SVGLoader();
+    loader = new SVGLoader();
 
     //Load letters
-    loader.load("./letters/A2-1.svg", (data) => {
-        //Get data in and out prepared
-        const paths = data.paths
-        const group = new THREE.Group()
-        //Extrusion settings
-        const extrudeSettings = {
-            curveSegments: 30,
-            bevelEnabled: false,
-            steps: 6,
-            depth: 3
-        }
+    var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for (var i = 0; i < str.length; i++) {
+        await createLetter(str[i])
+    }
 
-        //Loop through each path in the SVG (Should only be one)
-        for (var i = 0; i < paths.length; i++) {
-            //Get this path and make it shapes
-            const path = paths[i]
-            const shapes = path.toShapes(true);
-
-            //Material settings
-            const material = new THREE.MeshBasicMaterial({
-                color: 0xFFFFFF,
-                side: THREE.DoubleSide
-            })
-
-            //Make each shape a mesh
-			for ( let j = 0; j < shapes.length; j ++ ) {
-                const shape = shapes[j]
-                //const geometry = new THREE.ShapeBufferGeometry(shape)
-                const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings)
-                geometry.computeBoundingBox()
-                console.log(geometry.boundingBox)
-				const mesh = new THREE.Mesh(geometry, material)
-                group.add( mesh )
-			}
-        }
-
-        //Add to the group
-        scene.add(group)
-        renderer.render(scene, camera)
-    })
+    console.log(JSON.stringify(letterData, null, "\t"))
 
     //Done
     renderer.render(scene, camera)
+}
+
+function doGenerate(first, last) {
+    construction.first = "Daniel"
+    construction.last = "Royer"
+}
+
+async function sleep(millis) {
+    return new Promise((res, rej) => {
+        setTimeout(res, millis)
+    })
+}
+
+async function createLetter(letter) {
+    const group = new THREE.Group()
+    for (var i = 1; i <= Math.max(1, letterData[letter].max); i++) {
+        group.add(await createExtrusion(letter + Math.max(1, letterData[letter].max) + "-" + i))
+        //await sleep(1000)
+        var select = scene.getObjectByName(letter + Math.max(1, letterData[letter].max) + "-" + i)
+        scene.remove(select)
+    }
+    //console.log(letter)
+    //console.log(group)
+    const box = (new THREE.Box3()).setFromObject(group)
+    //console.log(box)
+    //console.log(box.max.x - box.min.x)
+    letterData[letter].width = box.max.x - box.min.x
+    //console.log(letterData)
+}
+
+async function createExtrusion(name) {
+    return new Promise((res, rej) => {
+        name = "./letters/" + name + ".svg"
+        loader.load(name, (data) => {
+            //Get data in and out prepared
+            const paths = data.paths
+            var group = new THREE.Group()
+            group.name = name
+            //Extrusion settings
+            const extrudeSettings = {
+                curveSegments: 30,
+                bevelEnabled: false,
+                steps: 6,
+                depth: 3
+            }
+
+            //Loop through each path in the SVG (Should only be one)
+            for (var i = 0; i < paths.length; i++) {
+                //Get this path and make it shapes
+                const path = paths[i]
+                const shapes = path.toShapes(true);
+
+                //Material settings
+                const material = new THREE.MeshStandardMaterial({
+                    color: 0x2150CE,
+                    side: THREE.DoubleSide,
+                    roughness: 0.9,
+                    emissive: 0x0f0f0f
+                })
+
+                //Make each shape a mesh
+                for ( let j = 0; j < shapes.length; j ++ ) {
+                    const shape = shapes[j]
+                    //const geometry = new THREE.ShapeBufferGeometry(shape)
+                    const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings)
+                    geometry.computeBoundingBox()
+                    const mesh = new THREE.Mesh(geometry, material)
+                    group.add( mesh )
+                }
+            }
+
+            //Add to the group
+            scene.add(group)
+            renderer.render(scene, camera)
+            res(group)
+        })
+    })
 }
 
 function animate() {
