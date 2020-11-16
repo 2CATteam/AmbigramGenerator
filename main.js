a8a2d657f505d01bd7a46331f9c9c4999a040185
Daniel Royer <2CATteam@gmail.com>
2020-11-15 20:23:46 -0600

    Added duplication for too-long mismatches

--- main.js
+++ main.js
@@ -233,8 +233,20 @@ async function doGenerate() {
 
     //Die if there is no solution
     if (sumScore("first") != sumScore("last")) {
-        alert("The generation cannot continue. Please choose different words.")
-        return
+        console.log("Duplication is necessary, unfortunately")
+        i = 0
+        while (sumScore("first") > sumScore("last")) {
+            var toAdd = {}
+            Object.assign(toAdd, construction.first[i])
+            construction.first.push(toAdd)
+            i++
+        }
+        while (sumScore("first") < sumScore("last")) {
+            var toAdd = {}
+            Object.assign(toAdd, construction.last[i])
+            construction.last.push(toAdd)
+            i++
+        }
     }
 
     //Call worker
@@ -281,57 +293,6 @@ function download() {
     var toSave = exporter.parse(scene)
     var blob = new Blob([toSave], {type: 'text/plain'})
     saveAs(blob, `${construction.firstWord}${construction.lastWord}.stl`)
-}
-
-async function createExtrusion(name, depth, pos, doSide, highQuality=false) {
-    return new Promise((res, rej) => {
-        name = "./letters/" + name + ".svg"
-        loader.load(name, async (data) => {
-            //Get data in and out prepared
-            const paths = data.paths
-            //Extrusion settings
-            const extrudeSettings = {
-                curveSegments: highQuality ? 30 : 7,
-                bevelEnabled: false,
-                steps: 2,
-                depth: depth
-            }
-
-            //Get this path and make it shapes
-            const path = paths[0]
-            const shapes = path.toShapes((name == "R1-1" ? false : true));
-
-            //Material settings
-            const material = new THREE.MeshPhongMaterial({
-                color: (doSide ? 0x500000 : 0x005000),
-                side: THREE.FrontSide,
-                //wireframe: true,
-                transparent: true,
-                opacity: .7,
-                emissive: 0x0f0f0f
-            })
-
-            //Make each shape a mesh
-            const shape = shapes[0]
-            const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings)
-            geometry.computeBoundingBox()
-            const mesh = new THREE.Mesh(geometry, material)
-
-            //Add to the group
-            scene.add(mesh)
-
-            if (doSide) {
-                mesh.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), Math.PI / 2)
-                mesh.translateX(pos - (construction.lastWidth ? construction.lastWidth : 0) / 2)
-                mesh.translateZ(-depth / 2)
-            } else {
-                mesh.translateX(pos - (construction.firstWidth ? construction.firstWidth : 0) / 2)
-                mesh.translateZ(-depth / 2)
-            }
-            render()
-            res(mesh)
-        })
-    })
 }
 
 //Animation loop
